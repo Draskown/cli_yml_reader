ee0d82c562c833326ab976572ddc4d6c04b3b696
Andryuha <astik452@gmail.com>
2023-05-14 01:20:30 +0300

    Folder exists tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,22 +5,15 @@ import yaml
 from typer.testing import CliRunner
 from program.app import list_entries, get_dependencies, get_entry, folder_correct
 
+def test_folder_correct_exists(tmp_path):
+    folder_path = tmp_path / "test_folder"
+    folder_path.mkdir()
+    assert folder_correct(str(folder_path)) == True
 
-# Test if list_entries() function works as expected
-def test_list_entries(tmp_path):
-    # Create a test file
-    with open(os.path.join(tmp_path, "tasks.yml"), "w") as f:
-        data = {"tasks": [{"name": "task1"}, {"name": "task2"}]}
-        yaml.dump(data, f)
-    # Test that list_entries() function returns the correct output
-    assert list_entries("task", str(tmp_path) + "/") == "- task1\n- task2\n"
+def test_folder_correct_not_exists(capsys):
+    assert folder_correct("non_existing_folder") == False
+    captured = capsys.readouterr()
+    assert captured.out == "Error: folder does not exists\n"
 
-
-# Test if get_dependencies() function works as expected
-def test_get_dependencies(tmp_path):
-    # Create a test file
-    with open(os.path.join(tmp_path, "tasks.yml"), "w") as f:
-        data = {"tasks": [{"name": "task1", "dependencies": ["task2", "task3"]}, {"name": "task2", "dependencies": []}]}
-        yaml.dump(data, f)
-    # Test that get_dependencies() function returns the correct output
-    assert get_dependencies("task1", str(tmp_path) + "/") == "task2, task3, task1, "
+def test_folder_correct_empty_path():    
+    assert folder_correct("") == True
